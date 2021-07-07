509b73848b91b1896df8782fe69c2c20e1f3cf74
Timofei Iurov <tim.riene@gmail.com>
2021-07-07 16:59:11 +0300

    7 lesson's hw is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    
+    let [posts, setPosts] = useState([
+      {
+        name: "First One",
+        isOpen: false,
+        description: "Some content #1",
+        likes: 0
+      },
+      {
+        name: "Second One",
+        isOpen: false,
+        description: "Some content #2",
+        likes: 0
+      },
+      {
+        name: "Third One",
+        isOpen: false,
+        description: "Some content #3",
+        likes: 0
+      }
+    ]);
+
+
+    // const postsChanged = [
+    //   {
+    //     name: "First One",
+    //     isOpen: true,
+    //     description: "Some content #1"
+    //   },
+    //   {
+    //     name: "Second One",
+    //     isOpen: false,
+    //     description: "Some content #2"
+    //   },
+    //   {
+    //     name: "Third One",
+    //     isOpen: false,
+    //     description: "Some content #3"
+    //   }
+    // ]
+
+    // const postInfo = {
+    //   title: "My Post Title",
+    //   description: "Some info about this post"
+    // };
+
+    const toggleHandler = (name: string) => {
+      // posts[0].isOpen = !posts[0].isOpen;
+      // setIsOpen(isOpen);
+      // console.log(isOpen);
+      let postsChanged: any = [...posts];
+      const el = postsChanged.find((el: any) => el.name === name);
+      el.isOpen = !el.isOpen;
+      console.log(el.isOpen);
+      setPosts(postsChanged);
+    };
+
+    const likesHandler = (name: string) => {
+      let postsChanged: any = [...posts];
+      const el = postsChanged.find((el: any) => el.name === name);
+      el.likes++;
+      console.log(el.isOpen);
+      setPosts(postsChanged);
+    };
+
+    
+
+//    !massive[0].isOpen
+
+    console.log("Rendering");
+
+    return (
+    <>
+      {posts.map((post) => (
+      <div className="post" key={ post.name }>
+        <div className="post__header">
+          <span className="post__title">{ post.name }</span>
+          <button className="post__toggler" onClick={() => toggleHandler(post.name)}>Toggle</button>
+          <button className="post__toggler" onClick={() => likesHandler(post.name)}>Like</button>
+          <span>{post.likes}</span>
+        </div>
+        {post.isOpen ? (
+        <div className="post__body">{ post.description }</div>
+        ) : null}
+      </div>
+      ))}
+    </>
+    );
+  };
 
 export default App;
